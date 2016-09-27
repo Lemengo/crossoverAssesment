d22bf4111036546a713e850aac5e00d7c1e3e09c
Project2travail <Project2travail@gmail.com>
2016-09-26 20:59:53 -0400

    New changes by Joel

--- create.pp
+++ create.pp
@@ -2,7 +2,7 @@ ec2_instance { 'Mengo-instance':
   ensure            => present,
   region            => 'us-east-1',
   availability_zone => 'us-east-1b',
-  image_id          => 'ami-6869aa05',
+  image_id          => 'ami-2d39803a',
   instance_type     => 't1.micro',
   security_groups   => ['default'],
    user_data         => template('/root/setup.sh'),
--- setupserver.sh
+++ setupserver.sh
@@ -25,22 +25,19 @@ docker pull php
 # run the containers
 
 echo "--> Running the docker instances"
-mkdir -p php-deploy && cd php-deploy 
+git clone https://github.com/Lemengo/crossoverAssesment.git  && cd crossoverAssesment 
 touch  ./Dockerfile
 echo "FROM php:7.0-apache" >> Dockerfile
-echo "COPY /var/www/html/ /var/www/html/" >> Dockerfile
-docker build -t php-deploy:Mengo-apache .
+docker build -t crossoverassesment/local:apache .
 cd ..
-git clone https://github.com/Lemengo/crossoverAssesment.git  && cd crossoverAssesment && docker build -t crossoverAssesment/local:apache .
-
 docker run -p 3700:3306 --name Mengo-mysql -e MYSQL_ROOT_PASSWORD=Lemengo12345 -d mysql:latest
-docker run -dit -p 90:80 --name Mengo-apache --link Mengo-mysql:mysql -v /var/www/html/:/var/www/html crossoverAssesment/local:apache
+docker run -dit -p 80:80 --name Mengo-apache --link Mengo-mysql:mysql -v /var/www/html/:/var/www/html crossoverassesment/local:apache
 
 #Backup directory
 mkdir backups 
 
-cp db.php index.php logout.php /var/www/html/
-cp dynamicronlog.sh /root && chmod +x /root/dynamicronlog.sh
-cp backup.sh /root && chmod +x /root/backup.sh
-echo '* 19 * * * /root/cronlogs.sh' | crontab -
-echo '* 19 * * * /root/backup.sh' | crontab -
+sudo cp db.php index.php logout.php /var/www/html/
+sudo cp dynamicronlog.sh /home/ubuntu && chmod +x /home/ubuntu/dynamicronlog.sh
+sudo cp backup.sh /home/ubuntu && chmod +x /home/ubuntu/backup.sh
+echo '* 19 * * * /home/ubuntu/dynamicronlog.sh' | crontab -
+echo '* 19 * * * /home/ubuntu/backup.sh' | crontab -
